fdeda9861c356acef13dad10497fbcd6df068053
joshryan3110 <94678406+joshryan3110@users.noreply.github.com>
2021-11-19 17:17:58 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+
+  createEdgeSprites();
+
+  //ball.bounceOff(edge);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    ball.position.x+=5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    ball.position.x-=5
+  }
+  if(keyIsDown(UP_ARROW)){
+    ball.position.y-=5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    ball.position.y+=5
+  }
+
+  drawSprites();
+}
+
+
+
+
